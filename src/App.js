5006b2b2ebd12c736fdea0b88dafade46319e158
ben <ben@localhost>
2024-03-07 13:18:48 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const options = [
+    { label: "Start", id: 1 },
+    { label: "Stop", id: 2 },
+    { label: "Pause", id: 3 },
+  ];
+
+  const createButtons = options.map((option) => (
+    <MyButton key={option.id} label={option.label} />
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my app.</h1>
+      <MyButton label="I'm a button" />
+      <MyButton label="I'm a button too" />
+      <MyButton />
+      {createButtons}
     </div>
   );
 }
 
-export default App;
+function MyButton({ label }) {
+  // Conditional button labeling
+  let buttonlabel;
+  if (label) {
+    buttonlabel = label;
+  } else {
+    buttonlabel = "Generic label";
+  }
+  // Set the count of button clicks to 0 initially
+  const [count, setCount] = useState(0);
+  // Whenever the handleClick function is called, increment count by 1
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return (
+    // {Create the button, labeling it with a label and count value}
+    <button onClick={handleClick}>{buttonlabel}: {count}</button>
+  );
+}
